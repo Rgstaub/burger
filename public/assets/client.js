6179933cb0db024f444e737f2517c9546a0c6809
Rgstaub <rgstaub@gmail.com>
2017-08-31 21:40:06 -0400

    ...

--- public/assets/client.js
+++ public/assets/client.js
@@ -2,11 +2,13 @@
 
 // create an on click event
 $(document).on('click', '.burger-wrap', function() {
+  event.preventDefault();
   console.log($(this).data("id"));
   // Post with a path that gives the Burger ID as a parameter ("/:id")
   $.post(`/_put/${$(this).data("id")}`, (req, res) => {
     
   })
+  location.reload();
 })
 
 
